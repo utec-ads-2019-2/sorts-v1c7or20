2774acd9336c372b3ed17253ac4db3fe1993e5bc
v1c7or20 <victorpa793@gmail.com>
2019-08-30 15:21:07 -0500

    radix

--- radix.h
+++ radix.h
@@ -8,7 +8,21 @@ class RadixSort : public Sort {
         RadixSort(int *elements, size_t size) : Sort(elements, size) {}
 
         void execute() {
-            // TODO
+            int max = *max_element(elements,elements+size),
+            sorted[size],
+            exp;
+            for ( exp = 1; max/exp > 0 ; exp= exp*10) {
+                int contador[10]={0};
+                for (int j = 0; j < size ; j++){
+                    contador[(elements[j]/exp)%10]++ ;}
+                for (int k = 1; k < 10 ; k++){
+                    contador[k] += contador[k - 1];}
+                for (int l = size-1; l >=0 ; l--){
+                    sorted[contador[(elements[l]/exp)%10]-1] = elements[l];
+                    contador[(elements[l]/exp)%10]-- ;}
+                for (int m = 0; m <size ; m++) {
+                    elements[m]=sorted[m];}
+            }
         }
 
         inline string name() { return "RadixSort"; }
