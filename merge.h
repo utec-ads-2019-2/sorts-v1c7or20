2ac3f652090256f3d1630ee88c1ef9745a0ab46c
v1c7or20 <victorpa793@gmail.com>
2019-08-31 23:21:01 -0500

    merge

--- merge.h
+++ merge.h
@@ -7,8 +7,46 @@ class MergeSort : public Sort {
     public:
         MergeSort(int *elements, size_t size) : Sort(elements, size) {}
 
+
+    void merge(int array[], int low, int high, int mid){
+        int i,j,k,temp[high-low+1];
+        i = low;
+        k=0;
+        j = mid + 1;
+        while (i<= mid && j <= high){
+            if (array[i] < array[j]){
+                temp[k]= array[i];
+                k++; i++;
+            }else{
+                temp[k] = array[j];
+                k++;
+                j++;
+            }
+        }
+        while (i <= mid){
+            temp[k] = array[i];
+            k++;
+            i++;
+        }
+        while (j <= high){
+            temp[k] = array[j];
+            k++;
+            j++;
+        }
+        for ( i = low; i <= high; i++) {
+            array[i] = temp[i-low];
+        }
+    }
+    void mergesort(int array[], int low, int high){
+        int mid = (low+high)/2;
+        if (low < high){
+            mergesort(array,low,mid);
+            mergesort(array,mid+1,high);
+            merge(array,low,high,mid);
+        }
+    }
         void execute() {
-            // TODO
+            mergesort(elements,0,size);
         }
 
         inline string name() { return "MergeSort"; }
